19baae6d978faee77d1575701d1ecee790af986b
jebsmartt <jeb.smartt@gmail.com>
2023-09-29 13:46:45 -0700

    Rename the view

--- game/views.py
+++ game/views.py
@@ -3,27 +3,7 @@ from django.http import HttpResponse
 from . import game_logic as gl
 
 # Create your views here.
-
-# def index(request):
-#     return HttpResponse("This is the HTTP Response for the Game app")
-
-def index(request):
-    # x = []
-    # for i in range(10):
-    #     x.append(i)
-
-    # # Generate the HTML for the table
-    # table_html = "<h1>Below this line there should be a table:</h1>"
-    # table_html += "<table border='1'><tr><th>Index</th><th>Value</th></tr>"
-
-    # for index, item in enumerate(x):
-    #     table_html += f"<tr><td>{index}</td><td>{item}</td></tr>"
-
-    # table_html += "</table>"
-
-    # return HttpResponse(table_html)
-
-
+def gameSession(request):
     testGame = gl.Game()
 
     # test data
