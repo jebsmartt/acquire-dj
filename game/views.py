d632b18a0855b56f5e35eb052593d9f223f16421
jebsmartt <jeb.smartt@gmail.com>
2023-09-29 14:55:26 -0700

    Connect view, template, and static css together

--- game/views.py
+++ game/views.py
@@ -4,6 +4,7 @@ from . import game_logic as gl
 
 # Create your views here.
 def gameSession(request):
+
     testGame = gl.Game()
 
     # test data
@@ -13,21 +14,36 @@ def gameSession(request):
 
     testGame.setup_game()
 
-    test_html = "<h1>Acquire! I hope...</h1>"
+    return render(request, 'game_board.html', {'testGame': testGame})
 
-    # Generate the HTML table for each player's tray
-    test_html += "<table border='1'>"
-    for player in testGame.players:
-        test_html += f"<tr><th>{player.username}</th></tr>"
-        for tile in player.tile_tray:
-            test_html += f"<tr><td>{tile.name}</td></tr>"
-    test_html +="</table>"
 
-    # Generate the HTML for the grid
-    test_html += "<h2>Below this line there should the grid:</h2>"
-    test_html += "<table border='1'>"
-    for space in testGame.grid.spaces:
-        test_html += f"<tr><td>{space.name}</td></tr>"
-    test_html +="</table>"
 
-    return HttpResponse(test_html)
+
+
+    # testGame = gl.Game()
+
+    # # test data
+    # testPlayers = ['Harry','Hermione','Ron']
+    # for player in testPlayers:
+    #     testGame.players.append(gl.Player(player))
+
+    # testGame.setup_game()
+
+    # test_html = "<h1>Acquire! I hope...</h1>"
+
+    # # Generate the HTML table for each player's tray
+    # test_html += "<table border='1'>"
+    # for player in testGame.players:
+    #     test_html += f"<tr><th>{player.username}</th></tr>"
+    #     for tile in player.tile_tray:
+    #         test_html += f"<tr><td>{tile.name}</td></tr>"
+    # test_html +="</table>"
+
+    # # Generate the HTML for the grid
+    # test_html += "<h2>Below this line there should the grid:</h2>"
+    # test_html += "<table border='1'>"
+    # for space in testGame.grid.spaces:
+    #     test_html += f"<tr><td>{space.name}</td></tr>"
+    # test_html +="</table>"
+
+    # return HttpResponse(test_html)
