1de217ca119d6fb5c29960843ca5d3d0c7f0150c
jebsmartt <jeb.smartt@gmail.com>
2023-09-28 22:12:48 -0700

    Add first version of grid as a table

---
+++ game/game_logic.py
@@ -0,0 +1,49 @@
+# Created to house logic for the game
+
+class Tile:
+    # Represents a tile that a player can play
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+        self.name = f'{row}{column}'
+
+class TileBag:
+    # Represents the storage for game tiles not in play
+    rows = ['A','B','C','D','E','F','G','H','I']
+    columns = [1,2,3,4,5,6,7,8,9,10]
+
+    def __init__(self):
+        self.contents = []
+
+    def fill_bag(self):
+        for r in TileBag.rows:
+            for c in TileBag.columns:
+                new_tile = Tile(r,c)
+                self.contents.append(new_tile)
+
+class Space:
+    # Represents spaces on the game board
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+        self.name = f'{row}{column}'
+
+class Grid:
+    # Represents the gameboard grid of spaces on which the player plays
+    rows = ['A','B','C','D','E','F','G','H','I']
+    columns = [1,2,3,4,5,6,7,8,9,10]
+
+    def __init__(self):
+        self.spaces = []
+
+    def set_grid(self):
+        for r in Grid.rows:
+            for c in Grid.columns:
+                space = Space(r,c)
+                self.spaces.append(space)
+
+    def print_grid(self):
+        pp_grid = []
+        for s in self.spaces:    
+            pp_grid.append(s.name)
+        print(pp_grid)
--- game/views.py
+++ game/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import game_logic as gl
 
 # Create your views here.
 
@@ -7,17 +8,32 @@ from django.http import HttpResponse
 #     return HttpResponse("This is the HTTP Response for the Game app")
 
 def index(request):
-    x = []
-    for i in range(10):
-        x.append(i)
+    # x = []
+    # for i in range(10):
+    #     x.append(i)
+
+    # # Generate the HTML for the table
+    # table_html = "<h1>Below this line there should be a table:</h1>"
+    # table_html += "<table border='1'><tr><th>Index</th><th>Value</th></tr>"
+
+    # for index, item in enumerate(x):
+    #     table_html += f"<tr><td>{index}</td><td>{item}</td></tr>"
+
+    # table_html += "</table>"
+
+    # return HttpResponse(table_html)
+
+
+    #instantiate the Grid
+    grid = gl.Grid()
+    grid.set_grid()
 
     # Generate the HTML for the table
-    table_html = "<h1>Below this line there should be a table:</h1>"
-    table_html += "<table border='1'><tr><th>Index</th><th>Value</th></tr>"
+    grid_html = "<table border='1'>"
 
-    for index, item in enumerate(x):
-        table_html += f"<tr><td>{index}</td><td>{item}</td></tr>"
+    for space in grid.spaces:
+        grid_html += f"<tr><td>{space.name}</td></tr>"
 
-    table_html += "</table>"
+    grid_html +="</table>"
 
-    return HttpResponse(table_html)
+    return HttpResponse(grid_html)
